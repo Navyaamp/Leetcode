f02ce0bb52b9cf94b1d1d4e71bc7843d7a8b13dc
Navyaamp <navyamp0@gmail.com>
2026-08-18 09:47:05 +0530

    Time: 0 ms (100%), Space: 60.4 MB (79.26%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    result=s.reverse()
+  return result;
+
+};
+console.log(reverseString(["h","e","l","l","o"]));
+
