b7992453e670acf04b38da1ddde50a4727e1515d
Navyaamp <navyamp0@gmail.com>
2026-08-29 15:23:54 +0530

    Time: 2 ms (16.98%), Space: 53.7 MB (89.43%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function(candies, extraCandies) {
+    let max = 0;
+    let result = [];
+
+    for (let i = 0; i < candies.length; i++) {
+        if (candies[i] > max) {
+            max = candies[i];
+        }
+    }
+    for (let i = 0; i < candies.length; i++) {
+        if (candies[i] + extraCandies >= max) {
+            result.push(true);
+        } else {
+            result.push(false);
+        }
+    }
+
+    return result;
+
+};
