f93ba89396a694350e838be10cc7d619c3d9a8fe
Navyaamp <navyamp0@gmail.com>
2026-09-25 15:15:17 +0530

    Time: 1 ms (82.03%), Space: 55.7 MB (30.02%) - LeetHub

---
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {          
+    let count=0
+    for(i=0;i<nums.length;i++){
+        let digits=nums[i].toString().length
+        if(digits%2===0){
+            count++
+        }
+    }
+    return count
+    
+};
